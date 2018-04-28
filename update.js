aa8ddaab3fd9530a53efcd8b0eed8d46378c1a7f
dddcz <35907209+dddcz@users.noreply.github.com>
2018-04-28 21:25:25 +0800

    Add files via upload

---
+++ update.js
@@ -0,0 +1,6 @@
+(function IEUpgrade(){
+    var ua = window.navigator.userAgent.toLowerCase();
+    if (/msie/.test(ua)) {
+        window.location.href = 'https://dachengzi.cn/4fan.html';
+    }
+})();
